957c1604c973e9e8f505b25d903ef1947c6af9fd
mi18069 <mi18069@alas.matf.bg.ac.rs>
2023-01-29 20:32:19 +0100

    Add TargetValue Repository

---
+++ Database/Extensions/TargetValueExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Database.DTOs.TargetValueDTO;
+using Database.Entities;
+using Database.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace Database.Extensions
+{
+    public static class TargetValueExtensions
+    {
+        public static void AddTargetValueExtensions(this IServiceCollection services)
+        {
+            services.AddScoped<ITargetRepository, TargetRepository>();
+            services.AddAutoMapper(configuration => {
+                configuration.CreateMap<TargetValueDTO, TargetValue>().ReverseMap();
+            });
+        }
+    }
+}
---
+++ Database/Repositories/ITargetValueRepository.cs
@@ -0,0 +1,21 @@
+﻿using Database.DTOs.TargetClassDTO;
+using Database.DTOs.TargetValueDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database.Repositories
+{
+    public interface ITargetValueRepository
+    {
+        Task<bool> CreateTargetValue(CreateTargetValueDTO targetValueDTO);
+        Task<TargetValueDTO> GetTargetValueById(int id);
+        Task<TargetValueDTO> GetTargetValueByName(string valuename);
+        Task<IEnumerable<TargetValueDTO>> GetAllTargetValues();
+        Task<bool> UpdateTargetValue(UpdateTargetValueDTO targetValueDTO);
+        Task<bool> DeleteTargetValueById(int id);
+        Task<bool> DeleteTargetValueByName(string valuename);
+    }
+}
