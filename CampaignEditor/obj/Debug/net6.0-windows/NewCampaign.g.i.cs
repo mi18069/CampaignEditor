538dd3ac151a2dc8835e19ee52ce3efa188db923
mi18069 <mi18069@alas.matf.bg.ac.rs>
2023-01-29 16:23:24 +0100

    automatic changes

--- CampaignEditor/obj/Debug/net6.0-windows/NewCampaign.g.i.cs
+++ CampaignEditor/obj/Debug/net6.0-windows/NewCampaign.g.i.cs
@@ -93,7 +93,7 @@ namespace CampaignEditor {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/CampaignEditor;component/newcampaign.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/CampaignEditor;V1.0.0.0;component/newcampaign.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\NewCampaign.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
