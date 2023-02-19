e5882108431af878dba3623dc2f3a6b1347c1dc4
mi18069 <mi18069@alas.matf.bg.ac.rs>
2023-02-19 19:40:13 +0100

    Added PricelistDTO

---
+++ Database/DTOs/PricelistDTO/PricelistDTO.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Database.DTOs.PricelistDTO
+{
+    public class PricelistDTO : BaseIdentityPricelistDTO
+    {
+        public PricelistDTO(int plid, int clid, string plname, int pltype, int sectbid, int seastbid, bool plactive, float price, float minprice, bool prgcoef, int pltarg, string a2chn, bool use2, int sectbid2, int sectb2st, int sectb2en, int valfrom, int valto, bool mgtype) : base(plid, clid, plname, pltype, sectbid, seastbid, plactive, price, minprice, prgcoef, pltarg, a2chn, use2, sectbid2, sectb2st, sectb2en, valfrom, valto, mgtype)
+        {
+        }
+    }
+}
--- Database/DTOs/UserDTO/BaseIdentityUserDTO.cs
+++ Database/DTOs/UserDTO/BaseIdentityUserDTO.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace CampaignEditor.DTOs.UserDTO
 {
     public class BaseIdentityUserDTO : BaseUserDTO
