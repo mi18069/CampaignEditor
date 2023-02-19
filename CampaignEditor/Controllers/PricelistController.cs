94a61793849cb20341ab3aed798169dd93a65cd6
mi18069 <mi18069@alas.matf.bg.ac.rs>
2023-02-19 21:12:39 +0100

    Added Pricelist Controller

---
+++ CampaignEditor/Controllers/PricelistController.cs
@@ -0,0 +1,48 @@
+﻿using CampaignEditor.DTOs.UserDTO;
+using CampaignEditor.Repositories;
+using Database.DTOs.PricelistDTO;
+using Database.Entities;
+using Database.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CampaignEditor.Controllers
+{
+    public class PricelistController
+    {
+        private readonly IPricelistRepository _repository;
+        public PricelistController(IPricelistRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+        public async Task<bool> CreatePricelist(CreatePricelistDTO pricelistDTO)
+        {
+            return await _repository.CreatePricelist(pricelistDTO);
+        }
+
+        public async Task<PricelistDTO> GetPricelistById(int id)
+        {
+            var pricelist = await _repository.GetPricelistById(id);
+            return pricelist;
+        }
+
+        public async Task<IEnumerable<PricelistDTO>> GetAllPricelists()
+        {
+            return await _repository.GetAllPricelists();
+        }
+
+        public async Task<bool> UpdatePricelist(UpdatePricelistDTO pricelistDTO)
+        {
+            return await _repository.UpdatePricelist(pricelistDTO);
+        }
+
+        public async Task<bool> DeletePricelistById(int id)
+        {
+            return await _repository.DeletePricelistById(id);
+        }
+    }
+}
