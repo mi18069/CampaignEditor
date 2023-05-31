9ad381b18a3446d6716816c7986959d51fd458d7
mi18069 <mi18069@alas.matf.bg.ac.rs>
2023-05-31 18:04:22 +0200

    Changed in config from comboBox to textBox

--- CampaignEditor/Config.xaml.cs
+++ CampaignEditor/Config.xaml.cs
@@ -15,39 +15,91 @@ namespace CampaignEditor
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            cbServer.Items.Add(".");
-            cbServer.Items.Add("localhost");
-            cbServer.SelectedIndex = 1;
 
             FillFields();
         }
 
         private void FillFields()
         {
-            Regex regPort = new Regex("port=\\d*;");
-            Regex regUsername = new Regex("user id=\\w*;");
-            Regex regPassword = new Regex("password=\\w*;");
-            Regex regDatabase = new Regex("database=\\w*;");
+            Regex regServer = new Regex("Server=[^;]*;");
+            Regex regPort = new Regex("port=[^;]*;");
+            Regex regUsername = new Regex("user id=[^;]*;");
+            Regex regPassword = new Regex("password=[^;]*;");
+            Regex regDatabase = new Regex("database=[^;]*;");
             string connectionString = AppSettings.ConnectionString;
-            
-            string portString = regPort.Match(connectionString).Value;
-            string portValue = portString.Substring(portString.IndexOf("=") + 1);
-            tbPort.Text = portValue.Remove(portValue.Length-1, 1);
-            string usernameString = regUsername.Match(connectionString).Value;
-            string usernameValue = usernameString.Substring(usernameString.IndexOf("=") + 1);
-            tbUsername.Text = usernameValue.Remove(usernameValue.Length - 1, 1);
-            string passwordString = regPassword.Match(connectionString).Value;
-            string passwordValue = passwordString.Substring(passwordString.IndexOf("=") + 1);
-            pbPassword.Password = passwordValue.Remove(passwordValue.Length - 1, 1);
-            string databaseString = regDatabase.Match(connectionString).Value;
-            string databaseValue = databaseString.Substring(databaseString.IndexOf("=") + 1);
-            tbDatabase.Text = databaseValue.Remove(databaseValue.Length - 1, 1);
+
+            try
+            {
+                string serverString = regServer.Match(connectionString).Value;
+                string serverValue = serverString.Substring(serverString.IndexOf("=") + 1);
+                tbServer.Text = serverValue.Remove(serverValue.Length - 1, 1);
+            }
+            catch
+            {
+                tbServer.Text = "";
+            }
+
+            try
+            {
+                string portString = regPort.Match(connectionString).Value;
+                string portValue = portString.Substring(portString.IndexOf("=") + 1);
+                tbPort.Text = portValue.Remove(portValue.Length - 1, 1);
+            }
+            catch
+            {
+                tbPort.Text = "";
+            }
+
+            try
+            {
+                string usernameString = regUsername.Match(connectionString).Value;
+                string usernameValue = usernameString.Substring(usernameString.IndexOf("=") + 1);
+                tbUsername.Text = usernameValue.Remove(usernameValue.Length - 1, 1);
+            }
+            catch
+            {
+                tbUsername.Text = "";
+            }
+
+            try
+            {
+                string passwordString = regPassword.Match(connectionString).Value;
+                string passwordValue = passwordString.Substring(passwordString.IndexOf("=") + 1);
+                pbPassword.Password = passwordValue.Remove(passwordValue.Length - 1, 1);
+            }
+            catch
+            {
+                pbPassword.Password = "";
+            }
+
+            try
+            {
+                string databaseString = regDatabase.Match(connectionString).Value;
+                string databaseValue = databaseString.Substring(databaseString.IndexOf("=") + 1);
+                tbDatabase.Text = databaseValue.Remove(databaseValue.Length - 1, 1);
+            }
+            catch
+            {
+                tbDatabase.Text = "";
+            }
+
         }
 
         private void btnConnect_Click(object sender, RoutedEventArgs e)
         {
+
+            if (tbServer.Text.Contains(';') ||
+                tbPort.Text.Contains(';') ||
+                tbUsername.Text.Contains(';') ||
+                pbPassword.Password.Contains(';') ||
+                tbDatabase.Text.Contains(';'))
+            {
+                MessageBox.Show("Property cannot contain symbol \";\"", "Message", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
             string connectionString = string.Format("Server={0};port={1};user id={2};password={3};database={4};",
-                cbServer.Text.Trim(), tbPort.Text.Trim(), tbUsername.Text.Trim(), pbPassword.Password.ToString().Trim(), tbDatabase.Text.Trim());
+                tbServer.Text.Trim(), tbPort.Text.Trim(), tbUsername.Text.Trim(), pbPassword.Password.ToString().Trim(), tbDatabase.Text.Trim());
 
             try
             {
@@ -63,8 +115,19 @@ namespace CampaignEditor
 
         private void btnSave_Click(object sender, RoutedEventArgs e)
         {
+
+            if (tbServer.Text.Contains(';') ||
+                tbPort.Text.Contains(';') ||
+                tbUsername.Text.Contains(';') ||
+                pbPassword.Password.Contains(';') ||
+                tbDatabase.Text.Contains(';'))
+            {
+                MessageBox.Show("Property cannot contain symbol \";\"", "Message", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
             string connectionString = string.Format("Server={0};port={1};user id={2};password={3};database={4};",
-                cbServer.Text.Trim(), tbPort.Text.Trim(), tbUsername.Text.Trim(), pbPassword.Password.ToString().Trim(), tbDatabase.Text.Trim());
+                tbServer.Text.Trim(), tbPort.Text.Trim(), tbUsername.Text.Trim(), pbPassword.Password.ToString().Trim(), tbDatabase.Text.Trim());
             
             // Saving without checkings
             AppSetting setting = new AppSetting();
