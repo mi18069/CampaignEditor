4cd1c79f4b21c0ce9bda181f9878dfa677ba779f
mi18069 <mi18069@alas.matf.bg.ac.rs>
2024-09-12 12:14:47 +0200

    Added expected spots for pairing

---
+++ CampaignEditor/UserControls/ValidationItems/PairSpotsItems/ExpectedSpotItem.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Database.DTOs.ChannelDTO;
+using Database.DTOs.SpotDTO;
+using System.Windows.Controls;
+
+namespace CampaignEditor.UserControls.ValidationItems.PairSpotsItems
+{
+    /// <summary>
+    /// Interaction logic for ExpectedSpotItem.xaml
+    /// </summary>
+    public partial class ExpectedSpotItem : UserControl
+    {
+        private SpotDTO _spot = null;
+        public ExpectedSpotItem(SpotDTO spot)
+        {
+            InitializeComponent();
+            _spot = spot;
+            SetControl(_spot);
+        }
+
+        private void SetControl(SpotDTO spot)
+        {
+            lblSpot.Text = $"{_spot.spotcode}: {_spot.spotname}";
+            lblSpotlength.Content = $"Length: {_spot.spotlength}";
+        }
+    }
+}
---
+++ CampaignEditor/UserControls/ValidationItems/PairSpotsItems/RealizedSpotItem.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace CampaignEditor.UserControls.ValidationItems.PairSpotsItems
+{
+    /// <summary>
+    /// Interaction logic for RealizedSpotItem.xaml
+    /// </summary>
+    public partial class RealizedSpotItem : UserControl
+    {
+        public RealizedSpotItem()
+        {
+            InitializeComponent();
+        }
+    }
+}
